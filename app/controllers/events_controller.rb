c194f24bd355ea28d408a47c534fe5278c738d45
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-09-23 12:46:33 -0300

    Setup controller for Event model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,20 @@
 class EventsController < ApplicationController
+  before_action :current_user
+
   def new
+    @event = current_user.events.new
   end
 
   def create
+    @event = current_user.events.create(event_params)
   end
 
   def show
+  end
+
+  private
+
+  def event_params
+    params.require(:events).permit(:description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root "sessions#welcome"
 
   resources :users, only: [:new, :create, :show]
+  resources :events, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
