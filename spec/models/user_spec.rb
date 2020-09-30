29ad3b698be46451d569efe6d17cc09f61a88363
mohammadumar28 <mohammadumar28@gmail.com>
2020-09-30 21:27:31 +0530

    Resolve space offences in Rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe User, type: :model do
   describe 'User associations' do
     it { should have_many(:events).with_foreign_key(:creator_id) }
     it { should have_many(:stats).with_foreign_key(:attendee_id) }
-    it { should have_many(:events_attended).through(:stats)}
+    it { should have_many(:events_attended).through(:stats) }
   end
 end
