ae980d0edf9b5aff55e41f050c2210e92e6040bd
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-10-02 13:28:11 -0300

    Fixing rubocop errors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -5,17 +5,17 @@ module EventsHelper
 
   def attend_event(event)
     return if event.creator.username != current_user.username
-      if event.stats.where('user_id = ?', current_user.id).length.zero?
-        link_to(
-          'Attend this event',
-          event_attend_path(
-            event,
-            user_id: current_user.id
-          ),
-          method: :post,
-          class: 'btn btn-info'
-        )
-      end
-    end
+
+    return unless event.stats.where('user_id = ?', current_user.id).length.zero?
+
+    link_to(
+      'Attend this event',
+      event_attend_path(
+        event,
+        user_id: current_user.id
+      ),
+      method: :post,
+      class: 'btn btn-info'
+    )
   end
 end
